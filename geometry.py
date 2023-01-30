2f6c3e5b53557ef004829e838b26e944b1f922e8
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-01-30 13:40:59 -0500

    specified instructionos

--- geometry.py
+++ geometry.py
@@ -1,2 +1,6 @@
-print("HELLO THIS IS ACHIEVEMENT 1 - TASK 1")
-
+#Make sure follow proper format Author date purpose
+#
+#
+#
+print("Welcome to the Geometry Calculator")
+print("1. Calculate the Area of a Circle \n2. Calculate the Area of a Rectangle \n3. Calculate the Area of a Triangle \n4. Quit")
